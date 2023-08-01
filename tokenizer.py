0037525222e2937fa649218cfe66a61ceb62780c
Luke <lukembeck@gmail.com>
2023-08-01 22:55:00 +1000

    Refactored tokenizer and corpus builders

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,39 @@
+import sys
+
 from sudachipy import tokenizer
 from sudachipy import dictionary
+
 
 tokenizer_obj = dictionary.Dictionary().create()
 tokenizer_mode = tokenizer.Tokenizer.SplitMode.C
 
 
-def tokenize(text):
-    tokens = [
-        " ".join("_".join([m.surface(), m.part_of_speech()[0], m.dictionary_form()]))
-        for m in tokenizer_obj.tokenize(text, tokenizer_mode)
-    ]
+def tokenize(content):
+    def _tokenize(input):
+        return tokenizer_obj.tokenize(input, tokenizer_mode)
 
-    return "\n".join(tokens)
+    if len(content) > 10000:  # Actual sudachipy tokenizer cutoff is 49149 bytes
+        return [token for span in content.split("\n") for token in _tokenize(span)]
+    else:
+        return _tokenize(content)
+
+
+def tag(tokens):
+    tag = lambda token: "_".join(
+        [token.surface(), token.part_of_speech()[0], token.dictionary_form()]
+    )
+
+    return " ".join([tag(token) for token in tokens])
+
+
+def build_word_list(tokens, word_list, word_index):
+    for t in tokens:
+        index_key = "-".join([t.dictionary_form()] + list(t.part_of_speech()))
+        if index_key not in word_index:
+            entry = [index_key, t.dictionary_form(), t.reading_form()] + list(
+                t.part_of_speech()
+            )
+            word_index[index_key] = True
+            word_list.append(entry)
+
+    return word_list, word_index
